06a017943d557ef2918a0e4af4bddc98dc7d2747
Niroigen Suntharam <niroigen@gmail.com>
2020-04-08 21:58:45 -0400

    Fixing spaces issue

--- test/test_main_memory.cpp
+++ test/test_main_memory.cpp
@@ -3,30 +3,30 @@
 
 TEST(MainMemory, init)
 {
-    MainMemory mem("memconfig.txt");
-    EXPECT_EQ(mem.getSize(), 2);
+  MainMemory mem("memconfig.txt");
+  EXPECT_EQ(mem.getSize(), 2);
 }
 
 TEST(MainMemory, writeAndWriteValid)
 {
-    MainMemory mem("memconfig.txt");
-    mem.write(0,500);
+  MainMemory mem("memconfig.txt");
+  mem.write(0,500);
 
-    EXPECT_EQ(mem.read(0), 500);
+  EXPECT_EQ(mem.read(0), 500);
 }
 
 TEST(MainMemory, writeInvalid)
 {
-    MainMemory mem("memconfig.txt");
-    mem.write(5,500);
+  MainMemory mem("memconfig.txt");
+  mem.write(5,500);
 
-    EXPECT_EQ(mem.getSize(), 2);
+  EXPECT_EQ(mem.getSize(), 2);
 }
 
 TEST(MainMemory, readInvalid)
 {
-    MainMemory mem("memconfig.txt");
-    mem.read(500);
+  MainMemory mem("memconfig.txt");
+  mem.read(500);
 
-    EXPECT_EQ(mem.getSize(), 2);
+  EXPECT_EQ(mem.getSize(), 2);
 }
