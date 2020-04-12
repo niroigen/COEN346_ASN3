779ea95fb007d50092fdf7fb1848d0b4eb015dd0
Niroigen Suntharam <niroigen@gmail.com>
2020-04-12 09:40:02 -0400

    Fixing process

--- src/include/process.hpp
+++ src/include/process.hpp
@@ -6,15 +6,18 @@
 #include "constants.hpp"
 
 struct Process {
-  Process(const Clock* _clk, unsigned int _start_time, unsigned int _burst_time, std::string cmd) {
+  Process(const Clock* _clk, unsigned int _start_time, unsigned int _burst_time, std::string _cmd) {
     clk = _clk;
     start_time = _start_time;
+    burst_time = _burst_time;
+    cmd = _cmd;
+    state = READY;
   }
   const Clock* clk;
   unsigned int start_time;
   unsigned int burst_time;
   std::string cmd;
-  std::string state = READY;
+  std::string state;
 };
 
 #endif // PROCESS_HPP
